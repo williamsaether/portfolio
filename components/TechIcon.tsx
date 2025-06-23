58f725ca50f591ea71c79e0d781f11d3e96a815b
William <william.saether03@gmail.com>
2025-06-23 15:59:18 +0200

    Add projects to frontpage

---
+++ components/TechIcon.tsx
@@ -0,0 +1,17 @@
+interface TechIconProps {
+	name: string;
+	color?: string;
+	className?: string;
+}
+
+export default function TechIcon({ name, color, className }: TechIconProps) {
+	if (name === "motion") return <img src="/images/icons/motion.svg" className={className ?? "size-4"} alt={name}></img>;
+	return <img
+		src={color ?
+			"https://cdn.simpleicons.org/" + name + "/" + color :
+			"https://cdn.simpleicons.org/" + name
+		}
+		className={className ?? "size-4"}
+		alt={name}
+	/>
+}
