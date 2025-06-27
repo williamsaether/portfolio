f3d9bc3a0d0bf0c60a04fe2f6bc6fae970229f68
William <william.saether03@gmail.com>
2025-06-27 16:36:23 +0200

    Simplify code

--- components/TechIcon.tsx
+++ components/TechIcon.tsx
@@ -5,9 +5,7 @@ interface TechIconProps {
 }
 
 export default function TechIcon({ name, color, className }: TechIconProps) {
-	if (name === "motion") return <img src="/images/icons/motion.svg" className={className ?? "size-4"} alt={name}></img>;
-	if (name === "java") return <img src="/images/icons/java.svg" className={className ?? "size-4"} alt={name}></img>;
-	if (name === "adobe") return <img src="/images/icons/adobe.svg" className={className ?? "size-4"} alt={name}></img>;
+	if (["motion","java","adobe"].includes(name)) return <img src={"/images/icons/" + name + ".svg"} className={className ?? "size-4"} alt={name}></img>;
 	return <img
 		src={color ?
 			"https://cdn.simpleicons.org/" + name + "/" + color :
