25f89fe55c3db8e386416be8b274eec516bc97ae
William Sæther <william.saether03@gmail.com>
2026-06-04 01:31:15 +0200

    fix: force dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -53,7 +53,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
 	return (
-		<html lang="en">
+		<html lang="en" className="dark">
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased scroll-smooth selection:bg-blue-950 selection:text-blue-400`}
 			>
